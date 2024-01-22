2531e058df6dadbc169744dcca0bdcb4da32affa
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 22:22:38 +0800

    Print values

--- current_and_previous_sum_numbers.py
+++ current_and_previous_sum_numbers.py
@@ -13,9 +13,10 @@ for current_number in range(0, 20, 2):
 
     # Create an arithemtic expression to express the sum of previous and current number
     sum = previous_number + current_number
-    
+
     # Print the values 
-    print(current_number)
+    print("The previous number is " + str(previous_number), "The current value of " + str(current_number) , "The sum is " + str(sum) ) 
+
     #Update the previous_number for the next iteration
     
    
