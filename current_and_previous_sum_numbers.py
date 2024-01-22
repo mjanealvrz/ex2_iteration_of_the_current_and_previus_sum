8bea78eb763d12e339f3f0ada04bf893d43f195b
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 22:10:57 +0800

    Set the previous number to 0

--- current_and_previous_sum_numbers.py
+++ current_and_previous_sum_numbers.py
@@ -6,7 +6,10 @@
 print("Printing current and previous number sum in a range(10)")
 
 # Set the previous number to 0
+previous_number = 0
+
 # # Create an loop for 10 numbers that is even numbers  (start = 0, stop = 20, step = 2)
+
     # Create an arithemtic expression to express the sum of previous and current number
     # Print the values 
     #Update the previous_number for the next iteration
