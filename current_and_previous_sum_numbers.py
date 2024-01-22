25e155c58df6e8302378eb2ec3453abba216686a
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 22:21:33 +0800

    Arithmethic expression for the sum

--- current_and_previous_sum_numbers.py
+++ current_and_previous_sum_numbers.py
@@ -10,7 +10,10 @@ previous_number = 0
 
 # Create an loop for 10 numbers that is even numbers  (start = 0, stop = 20, step = 2)
 for current_number in range(0, 20, 2):
+
     # Create an arithemtic expression to express the sum of previous and current number
+    sum = previous_number + current_number
+    
     # Print the values 
     print(current_number)
     #Update the previous_number for the next iteration
