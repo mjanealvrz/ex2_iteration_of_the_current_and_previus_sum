8de8bbabe83e5a7a015b14cde8c964d12b11036f
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 22:05:34 +0800

    Created pseudocode or the flow of the program

--- current_and_previous_sum_numbers.py
+++ current_and_previous_sum_numbers.py
@@ -1,2 +1,16 @@
 #Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
+#Pseudocode
+
+# Set a starting point
+# Set the previous number to 0
+# # Create an loop for 10 numbers that is even numbers  (start = 0, stop = 20, step = 2)
+    # Create an arithemtic expression to express the sum of previous and current number
+    # Print the values 
+    #Update the previous_number for the next iteration
+    
+   
+        
+    
+
+
